4304bd4fa08829ec4974d23d3c682e001a04abe7
Lucas Inocencio <lucas-inocencio@users.noreply.github.com>
2023-03-14 23:49:35 -0300

    The Beginning

---
+++ src/2-2.py
@@ -0,0 +1,11 @@
+from math import pi
+# 1. The volume of a sphere with radius r is 4/3πr3. What is the volume of a sphere with radius 5?
+print(4/3*pi*5**3)
+# 523.6
+
+# 2. Suppose the cover price of a book is $24.95, but bookstores get a 40% discount. Shipping costs $3 for the first copy and 75 cents for each additional copy. What is the total wholesale cost for 60 copies?
+print(24.95*60*0.6+3+0.75*59)
+# 945.45
+
+# 3. If I leave my house at 6:52 am and run 1 mile at an easy pace (8:15 per mile), then 3 miles at tempo (7:12 per mile) and 1 mile at an easy pace again, what time do I get home for breakfast
+# 
